00991adb68d1c2d10e6ec6c01dcb7878c1ad9cf4
Nicholas Berlette <nick@berlette.com>
2021-10-18 08:48:05 -0700

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,28 @@
 <script>
-	export let name;
+	export let name = 'svelte.run'
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>{name}</h1>
+	<p class="bottom">
+		deployed with vercel &middot;  <a href="https://github.com/svelterun">github</a>
+	</p>
 </main>
 
 <style>
+	::selection {
+		background-color: transparent;
+		cursor: default;
+		color: inherit;
+	}
+	.bottom {
+		position: absolute;
+		bottom: 2em;
+		width: 100%;
+		display: block;
+		left: 0;
+		text-align: center;
+	}
 	main {
 		text-align: center;
 		padding: 1em;
@@ -17,9 +32,16 @@
 
 	h1 {
 		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
+		text-transform: lowercase;
+		font-size: 6em;
 		font-weight: 100;
+		cursor: default;
+		display: block;
+		width: 100%;
+		text-align: center;
+		position: absolute;
+		top: 0;
+		left: 0;
 	}
 
 	@media (min-width: 640px) {
